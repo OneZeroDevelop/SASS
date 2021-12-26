b9f112a2f825bb96cce79141d4d960e3c18f7f81
OneZeroDevelop <krog2008@yandex.ru>
2021-12-26 22:55:17 +0300

    changes in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 function sendFunction() {
 	alert('Отправлено');
 }
+function takeFunction() {
+	alert('Принято')
+}
+function addFunction() {
+	alert ('Еще одна функция')
+}
 
 console.log('test 2')
 console.log('test 3')
